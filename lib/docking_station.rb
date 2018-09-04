1951e3ea5e6a5f9207deab9e8b5b6588d3b242e1
Ibra <ibra@MacBook-Pro-6.local>
2018-09-04 17:30:35 +0100

    chap 11 complete, concept of docking bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,13 @@
 require "bike.rb"
 class DockingStation
-
+  attr_reader :bike
   def release_bike
     Bike.new
   end
-  
+  def dock_bike(bike)
+    @bike = bike
+  end
+  def check_bike
+    @bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,24 @@
 require 'docking_station.rb'
 describe DockingStation do
-  let(:docking_station){DockingStation.new}
-
-  it "responds to release_bike method" do
-    expect(docking_station).to respond_to(:release_bike)
+  it { is_expected.to respond_to :release_bike}
+  it "releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
+  describe 'dock_bike' do
+    it { is_expected.to respond_to(:dock_bike).with(1).argument}
+    it "docks a bike" do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq bike
 
+    end
+  end
+  describe 'check_bike' do
+    it { is_expected.to respond_to(:check_bike)}
+    it "returns the docked bike" do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.check_bike).to eq bike
+    end
+  end
 end
